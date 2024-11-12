514ad3707aaea2cd993d730eab8158cc69700166
guillermoroman <guillermoroman@outlook.com>
2024-11-12 13:06:58 +0100

    Añadir insertarBebida

--- src/main/java/Prueba1.java
+++ src/main/java/Prueba1.java
@@ -12,15 +12,59 @@ public class Prueba1 {
                 System.out.println("Conexión realizada");
             }
 
-            //Ejecutar consultas
+            // Ejecutar consultas
             mostrarPizzas(connection);
             mostrarBebidas(connection);
+
+            // Insertar una nueva pizza
+            insertarPizza(connection, "Pizza Margarita", 8.5f, 13.9f, "Tomate, Mozzarella, Albahaca");
+            insertarBebida(connection, "Zumo", 5.0f);
 
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private static void insertarPizza(Connection connection, String nombre, float coste, float precio, String ingredientes) {
+        String query = "INSERT INTO pizzas (nombre, coste, precio, ingredientes) VALUES (?, ?, ?, ?)";
+        try(PreparedStatement pstmt = connection.prepareStatement(query)){
+            pstmt.setString(1, nombre);
+            pstmt.setFloat(2, coste);
+            pstmt.setFloat(3, precio);
+            pstmt.setString(4, ingredientes);
+
+            int rowsInserted = pstmt.executeUpdate();
+            if(rowsInserted > 0){
+                System.out.println("Pizza " + nombre + " añadida con éxito");
+            } else {
+                System.out.println("Impresión desde else");
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("Error al insertar la pizza");
+        }
+    }
+
+    private static void insertarBebida(Connection connection, String nombre, float precio) {
+        String query = "INSERT INTO bebidas (nombre, precio) VALUES (?, ?)";
+        try(PreparedStatement pstmt = connection.prepareStatement(query)){
+            pstmt.setString(1, nombre);
+            pstmt.setFloat(2, precio);
+
+            int rowsInserted = pstmt.executeUpdate();
+            if(rowsInserted > 0){
+                System.out.println("Bebida " + nombre + " añadida con éxito");
+            } else {
+                System.out.println("Impresión desde else");
+            }
+
+
+        } catch (SQLException e) {
+            System.out.println("Error al insertar la pizza");
+        }
     }
 
     private static void mostrarBebidas(Connection connection) throws SQLException {
